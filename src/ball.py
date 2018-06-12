c1a641c43771ce5dd5a51e07557c6609bbb74748
briandoyle81 <briandoyle81@gmail.com>
2018-06-12 15:28:26 -0600

    kinetic balls mostly working

--- src/ball.py
+++ src/ball.py
@@ -1,4 +1,6 @@
-from pygame.math import Vector2 as Vector2
+import math
+
+from pygame.math import Vector2
 
 class Ball:
     """
@@ -13,13 +15,12 @@ class Ball:
 
     def update(self):
         # bounce at edges
-        # account for decimal in position
+        # account for decimal in position # TODO:  Faster to use < and >
         if int(self.position.x) not in range(0 + self.radius, self.bounds[0] - self.radius): # screen width
             self.velocity.x *= -1
         if int(self.position.y) not in range(0 + self.radius, self.bounds[1] - self.radius): # screen height
             self.velocity.y *= -1
-        self.position.x += self.velocity.x
-        self.position.y += self.velocity.y
+        self.position += self.velocity
 
     def draw(self, screen, pygame):
         # cast x and y to int for drawing
@@ -52,17 +53,59 @@ class KineticBall(Ball):
     """
     A ball that collides with other collidable balls using simple elastic circle collision
     """
-    def __init__(self, object_list):
+    def __init__(self, mass, object_list, bounds, position, velocity, color, radius):
         self.object_list = object_list
+        self.mass = mass
+        print(position)
+        super().__init__(bounds, position, velocity, color, radius)
+
+    def collide(self, object, relative_vector):
+        print('bang!')
+        
+        # We can imagine the point of reflection as a wall tangent to the collision 
+        tangent = math.atan2(relative_vector.y, relative_vector.x)
+        # Get the angle of travel for both
+        angle1 = 0.5 * math.pi - math.atan2(self.velocity.y, self.velocity.x)
+        angle2 = 0.5 * math.pi - math.atan2(object.velocity.y, object.velocity.x)
+        # The angles of travel are updated to be two times the tangent minus the current angle
+        angle1 = 2 * tangent - angle1
+        angle2 = 2 * tangent - angle2
+
+        # Exchange speed
+        # Get velocity of other particle
+        object_speed = object.velocity.length()
+        self_speed = self.velocity.length()
+
+        # Update with new angle and opposing particle's speed
+        self.velocity = Vector2(math.sin(angle1), math.cos(angle1)) * object_speed
+        object.velocity = Vector2(math.sin(angle2), math.cos(angle2)) * self_speed
+
+        # Fix sticky problem
+        angle = 0.5 * math.pi + tangent
+        self.position.x += math.sin(angle)
+        self.position.y -= math.cos(angle)
+        object.position.x -= math.sin(angle)
+        object.position.y += math.cos(angle)
+
+        # # TODO:  This is not efficient
+        # while relative_vector.length() <= self.radius + object.radius:
+        #     self.position += self.velocity
+        #     object.position += object.velocity
+        #     relative_vector = self.position - object.position
+
 
     def update(self):
+        print(self.velocity)
         # Warning!:  This is a primitive method of collision detection
         # Consider time complexity when adding more of this type
-        for object in self.object_list:
-            if object != self and type(object) is KineticBall: # Don't collide with itself or non kinetic
-                relativeVector = Vector2(self.position.x - object.x, self.position.y - object.y)
-                if relativeVector.length() <= self.radius + object.radius:
+        index = self.object_list.index(self)
+        for object in self.object_list[index+1:]:  # TODO: Check effeciency
+            if type(object) is KineticBall and object != self: # Don't collide with non kinetic
+                # import pdb; pdb.set_trace()
+                relative_vector = self.position - object.position
+                if relative_vector.length() <= self.radius + object.radius:
                     # We are in collision range, so collide
-                    pass
+                    self.collide(object, relative_vector)
 
+        super().update()
 
--- src/draw.py
+++ src/draw.py
@@ -18,20 +18,41 @@ def main():
  
     object_list = [] # list of objects of all types in the toy
 
-    ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
-    object_list.append(ball)
+    # ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
+    # object_list.append(ball)
 
-    bouncing_ball = BouncingBall(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 0),
-                                    [0, 0, 255], 8)
-    object_list.append(bouncing_ball)
+    # bouncing_ball = BouncingBall(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 0),
+    #                                 [0, 0, 255], 8)
+    # object_list.append(bouncing_ball)
 
-    rainbow_ball = RainbowBall(SCREEN_SIZE, Vector2(400, 300), Vector2(3, -2),
-                                    [0, 255, 0], 8)
-    object_list.append(rainbow_ball)
+    # rainbow_ball = RainbowBall(SCREEN_SIZE, Vector2(400, 300), Vector2(3, -2),
+    #                                 [0, 255, 0], 8)
+    # object_list.append(rainbow_ball)
 
-    bouncing_rainbow_ball = BouncingRainbow(SCREEN_SIZE, Vector2(300, 150),
-                                    Vector2(-4, 0), [0, 255, 0], 8)
-    object_list.append(bouncing_rainbow_ball)
+    # bouncing_rainbow_ball = BouncingRainbow(SCREEN_SIZE, Vector2(300, 150),
+    #                                 Vector2(-4, 0), [0, 255, 0], 8)
+    # object_list.append(bouncing_rainbow_ball)
+    
+    
+    # kinetic1 = KineticBall(1, object_list, SCREEN_SIZE, Vector2(50, 100),
+    #                                     Vector2(2, 0), [10, 255, 0], 20)
+    # kinetic2 = KineticBall(1, object_list, SCREEN_SIZE, Vector2(500, 103),
+    #                                     Vector2(-1, 0), [10, 255, 0], 20)
+
+    # kinetic1 = KineticBall(1, object_list, SCREEN_SIZE, Vector2(300, 100),
+    #                                     Vector2(0, 2), [10, 255, 0], 20)
+    # kinetic2 = KineticBall(1, object_list, SCREEN_SIZE, Vector2(300, 408),
+    #                                     Vector2(0, -1), [10, 255, 0], 20)
+
+    # object_list.append(kinetic1)
+    # object_list.append(kinetic2)
+
+    for i in range(1, 10):
+        kinetic = KineticBall(1, object_list, SCREEN_SIZE, 
+                                        Vector2(random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
+                                        Vector2(4*random.random() - 2, 4*random.random() - 2),
+                                        [10, 255, 0], 20)
+        object_list.append(kinetic)
  
     while True: # TODO:  Create more elegant condition for loop
         for event in pygame.event.get():
