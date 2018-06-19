8b0cd35d2bef2758d21c8b19dc5a4c6f404e3b31
Unknown <opticalfalicy@gmail.com>
2018-06-19 17:36:02 -0500

    Balls, Squares

--- src/ball.py
+++ src/ball.py
@@ -17,7 +17,7 @@ class Ball:
         # bounce at edges.  TODO: Fix sticky edges
         if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
             self.velocity.x *= -1
-        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
+        if self.position.y < 100 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
             self.velocity.y *= -1
         self.position += self.velocity
 
@@ -25,29 +25,57 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball):
+    """
+    ball effected by gravity
+    """
+   
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+    GRAVITY = .1
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+    def update(self):  # TODO: Rounding Error
+        # print('called update')
+        self.velocity.y += self.GRAVITY
+        super().update()
 
-# class KineticBall(???):
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+   # TODO:
+    def update(self):
+        r = (self.color[0] + 10) % 256
+        g = (self.color[1] - 5) % 256
+        b = (self.color[2] + 5) % 256
+        self.color = [r, g, b]
+        super().update()
+
+class BouncingRainbow(RainbowBall, BouncingBall):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    # TODO:
+    pass
+
+# class KineticBall(BouncingRainbow):
 #     """
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     """
 #     # TODO:
+#     def update(self):
+#         p1 = self.position
+#         p2 = self.position
+#         v1 = self.velocity
+#         v2 = self.velocity
+#         b1 = self.bounds
+#         b2 = self.bounds
+
+#         if p1.x == p2.x or p1.y == p2.y:
+#             v1.x += 5
+#             v1.y += 5
+#             v2.x += 3
+#             v2.y += 3
+#             super().update()
 
 # class KineticBouncing(???):
 #     """
--- src/draw.py
+++ src/draw.py
@@ -2,18 +2,37 @@ import pygame #TODO:  Fix intellisense
 import random
 
 from pygame.math import Vector2
+# from tkinter import *
 
 from ball import *
+from square import *
 
 SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
 
 def debug_create_objects(object_list):
-    ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
-    object_list.append(ball)
+    for i in range(5):
+            ball = BouncingBall(SCREEN_SIZE, Vector2(random.randint(100, 400),
+                                                    random.randint(100, 400)),
+                                                    Vector2(random.random(), random.random()),
+                                                    [255, 0, 0], 10)
+            object_list.append(ball)
 
     # TODO: Create other ball types for testing
-  
+    square = FallingSq(SCREEN_SIZE, Vector2(50, 50), Vector2(0, 0), [0, 255, 0], 50, 50)
+    object_list.append(square)
+    square = FallingSq(SCREEN_SIZE, Vector2(600, 50), Vector2(0, 0), [0, 255, 0], 50, 50)
+    object_list.append(square)
+    square = FallingSq(SCREEN_SIZE, Vector2(300, 50), Vector2(0, 0), [0, 255, 0], 50, 50)
+    object_list.append(square)
+
+    square = RisingSq(SCREEN_SIZE, Vector2(200, 450), Vector2(0, 0), [0, 255, 0], 50, 50)
+    object_list.append(square)
+
+    square = BallingSq(SCREEN_SIZE, Vector2(400, 400), Vector2(0, 0), [0, 255, 0], 50, 50)
+    object_list.append(square)
+    
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -34,6 +53,11 @@ def main():
             if event.type == pygame.KEYDOWN:  #TODO:  Get working
                 if event.key == pygame.K_SPACE:
                     # TODO: Add behavior when button pressed
+                    print('click')
+                    ball = BouncingRainbow(SCREEN_SIZE, Vector2(random.randint(100, 400),
+                                                    random.randint(100, 400)),
+                                                    Vector2(random.random(), random.random()),
+                                                    [255, 0, 0], 10)
                     pass
 
         for ball in object_list:
