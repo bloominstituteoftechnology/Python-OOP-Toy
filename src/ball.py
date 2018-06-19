f37e69e81258f168df06200eae2622aa2469f0fa
unknown <ruiz.james12@yahoo.com>
2018-06-19 15:55:49 -0700

    James Ruiz Initial Commit

--- src/ball.py
+++ src/ball.py
@@ -25,17 +25,27 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball):
+    """
+    ball effected by gravity
+    """
+    # TODO: 
+    GRAVITY = .1
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+    def update(self):
+        self.velocity.y += self.GRAVITY
+        super().update()
+
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    def update(self):
+        r = (self.color[0] + 10) % 256
+        g = (self.color[1] - 5) % 256
+        b = (self.color[2] + 5) % 256
+        self.color = [r, g, b]
+        super().update()
 
 # class BouncingRainbow(???):
 #     """
