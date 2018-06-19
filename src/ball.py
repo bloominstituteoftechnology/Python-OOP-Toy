34593aac7c5cb78e581f1e2dcedf69b2bda22dd6
Drew Moody <drew.moody@me.com>
2018-06-19 17:46:27 -0400

    implemented BouncingBall, RainbowBall, and BouncingRainbow

--- src/ball.py
+++ src/ball.py
@@ -25,23 +25,38 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball): # this inherits from the ball class
+    """
+    ball effected by gravity
+    """
+    # TODO: 
+    GRAVITY = 0.1
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+    def update(self): #TODO Fix rounding error that makes it bounce higher
+        #this function will override the update function inherited from Ball
+        self.velocity.y += self.GRAVITY
+        # Now that you've done your class specific stuff, call your parent's update function
+        super().update()
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    # TODO:
+    def update(self):
+        r = (self.color[0] + 10) % 256
+        g = (self.color[1] - 5) % 256
+        b = (self.color[2] + 5) % 256
+        self.color = [r, g, b]
+
+        super().update()
+
+class BouncingRainbow(BouncingBall, RainbowBall): # notice we're inheriting from BOTH rainbow and bouncing ball
+    """
+    Ball that changes color and is affected by gravity
+    """
+    # TODO:
+
 
 # class KineticBall(???):
 #     """
