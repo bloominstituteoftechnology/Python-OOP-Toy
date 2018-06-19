d8dcc4d7eb37ab25c35e44133481671e80036414
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-19 17:38:27 -0400

    separate file created for rectangles - works
    All instances (BouncingRect, RainbowRect, and BouncingRainbow) seem to be rendering correctly.

--- src/rect/rect.py
+++ src/rect/rect.py
@@ -2,53 +2,54 @@ import math
 
 from pygame.math import Vector2
 
-class Ball:
+class Rect:
     """
     base class for bouncing objects
     """
     # Functions just like a JS constructor except...(FINISH) - about 50 min in
-    def __init__(self, bounds, position, velocity, color, radius):
+    def __init__(self, bounds, position, velocity, color, width, height):
         self.position = position
         self.velocity = velocity
         self.bounds = bounds
         self.color = color
-        self.radius = radius
-        number = 42
-        self.derivedValue = radius * 42
+        self.width = width
+        self.height = height
+        # number = 42
+        # self.derivedValue = radius * 42
 
     def update(self):
         # bounce at edges.  TODO: Fix sticky edges
-        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
+        if self.position.x < 0 or self.position.x > self.bounds[0] - self.width:# screen width
             self.velocity.x *= -1
-        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
+        if self.position.y < 0 or self.position.y > self.bounds[1] - self.height: # screen height
             self.velocity.y *= -1
         self.position += self.velocity
 
     def draw(self, screen, pygame):
         # cast x and y to int for drawing
-        pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
+        pygame.draw.rect(screen, self.color, ( self.position.x, self.position.y, self.width, self.height))
 
         '''
         # html
         <div class = 'coolClass
         '''
-# It may be tempting to just copy and paste the above Ball class and alter it, but this is redundant and
+# It may be tempting to just copy and paste the above Rect class and alter it, but this is redundant and
 # not convention.  Instead, use inheritance!
-# Example Exercise: Pass `Ball` in as an argument into `BouncingBall`, and change the `ball` constructor
-# in draw.py to `BouncingBall` instead of `Ball`. -- #QUESTION: Ask for more clarity on this!?!?
-class BouncingBall(Ball): # Passing Ball in makes BouncingBall a child of Ball
+# Example Exercise: Pass `Rect` in as an argument into `BouncingRect`, and change the `Rect` constructor
+# in draw.py to `BouncingRect` instead of `Rect`. -- #QUESTION: Ask for more clarity on this!?!?
+class BouncingRect(Rect): # Passing Rect in makes BouncingRect a child of Rect
     """ 
-    ball effected by gravity
+    Rect effected by gravity
     """
     # TODO: 
-    # If you look in Ball class, you will notice that the velocity is defined as `Vector2(3, 3)`
+    # If you look in Rect class, you will notice that the velocity is defined as `Vector2(3, 3)`
     # # Vector2 is just a class that holds an x and y property for us.  Does common calculations
     # # such as determining distance etc.  See methods here: https://www.pygame.org/docs/ref/math.html#pygame.math.Vector2
     GRAVITY = .1
 
-    # We need to override the update function as it appears in class Ball
-    def update(self): #TODO: Fix rounding error that makes ball bounce higher over time
-        # print('called update in BouncingBall') # No more bouncingBall
+    # We need to override the update function as it appears in class Rect
+    def update(self): #TODO: Fix rounding error that makes Rect bounce higher over time
+        # print('called update in BouncingRect') # No more bouncingRect
         # upda tes update function y velocity values
         self.velocity.y += self.GRAVITY
 
@@ -58,9 +59,9 @@ class BouncingBall(Ball): # Passing Ball in makes BouncingBall a child of Ball
         super().update()
     
 
-class RainbowBall(Ball):
+class RainbowRect(Rect):
     """
-    Ball that changes colors
+    Rect that changes colors
     """
     # TODO: 
     def update(self):
@@ -70,17 +71,17 @@ class RainbowBall(Ball):
         g = (self.color[1] - 1) % 256
         b = (self.color[2] + 0.5) % 256
         self.color = [ r, g, b ]
-        # This will get the balls moving!
+        # This will get the Rects moving!
         super().update()
 
-class BouncingRainbow(BouncingBall, RainbowBall): # That's it!  Everything you need is already in the previously constructed instances!
+class BouncingRainbow(BouncingRect, RainbowRect): # That's it!  Everything you need is already in the previously constructed instances!
     """
-    Ball that changes color and is affected by gravity
+    Rect that changes color and is affected by gravity
     """
 
-# class BouncingRectangles(BouncingBall):
+# class BouncingRectangles(BouncingRect):
 #     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
+#     A Rect that collides with other collidable Rects using simple elastic circle collision
 #     """
 #     # TODO:
 #     def draw(self):
@@ -88,20 +89,20 @@ class BouncingRainbow(BouncingBall, RainbowBall): # That's it!  Everything you n
 #         super().draw()
 
 
-class KineticBall(BouncingBall):
+class KineticRect(BouncingRect):
     """
-    A ball that collides with other collidable balls using simple elastic circle collision
+    A Rect that collides with other collidable Rects using simple elastic circle collision
     """
     # TODO:
 
 # class KineticBouncing(???):
 #     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
+#     A Rect that collides with other collidable Rects using simple elastic circle collision
 #     And is affected by gravity
 #     """
     
 
 # class AllTheThings(???):
 #     """
-#     A ball that does everything!
+#     A Rect that does everything!
 #     """
