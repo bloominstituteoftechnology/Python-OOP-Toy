79097c236517981a787901d034b78801f139e0ab
Khepf <jeremy1975@gmail.com>
2018-06-19 15:29:05 -0400

    first commit

--- src/ball.py
+++ src/ball.py
@@ -2,10 +2,12 @@ import math
 
 from pygame.math import Vector2
 
+
 class Ball:
     """
     base class for bouncing objects
     """
+
     def __init__(self, bounds, position, velocity, color, radius):
         self.position = position
         self.velocity = velocity
@@ -15,33 +17,52 @@ class Ball:
 
     def update(self):
         # bounce at edges.  TODO: Fix sticky edges
-        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
+        # screen width
+        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius:
             self.velocity.x *= -1
-        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
+        # screen height
+        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius:
             self.velocity.y *= -1
         self.position += self.velocity
 
     def draw(self, screen, pygame):
         # cast x and y to int for drawing
-        pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
+        pygame.draw.circle(screen, self.color, [int(
+            self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+class BouncingBall(Ball):
+    """
+    ball effected by gravity
+    """
+    # TODO:
+    GRAVITY = .1
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+    def update(self):
+        self.velocity.y += self.GRAVITY
+        super().update()
+
+
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    # TODO:
+
+    def update(self):
+        r = (self.color[0] + 10) % 256
+        g = (self.color[1] - 5) % 256
+        b = (self.color[2] + 5) % 256
+        self.color = [r, g, b]
+        super().update()
+
+
+class BouncingRainbow(BouncingBall, RainbowBall):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    # TODO:
+    pass
 
 # class KineticBall(???):
 #     """
@@ -54,7 +75,7 @@ class Ball:
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     And is affected by gravity
 #     """
-    
+
 
 # class AllTheThings(???):
 #     """
