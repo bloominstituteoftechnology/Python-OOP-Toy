6bd11ec80dda89eb67f6a378b7082d677e8f652e
AmyShackles <amyshackles@gmail.com>
2018-07-11 15:49:22 -0700

    Created BouncingBall
    - Created RainbowBall
    - Createed BouncingRainbow
    - Created KineticBall
    - Working on sticky edges

--- src/ball.py
+++ src/ball.py
@@ -2,59 +2,90 @@ import math
 
 from pygame.math import Vector2
 
+
 class Ball:
     """
     base class for bouncing objects
     """
     def __init__(self, bounds, position, velocity, color, radius):
+        self.bounds = bounds
         self.position = position
         self.velocity = velocity
-        self.bounds = bounds
         self.color = color
         self.radius = radius
 
     def update(self):
-        # bounce at edges.  TODO: Fix sticky edges
-        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
+        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius:  # screen width
             self.velocity.x *= -1
-        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
-            self.velocity.y *= -1
+        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius:  # screen height
+                self.velocity.y *= -1
         self.position += self.velocity
 
     def draw(self, screen, pygame):
         # cast x and y to int for drawing
-        pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
+        pygame.draw.circle(
+            screen, self.color, [int(self.position.x),
+                                 int(self.position.y)], self.radius)
 
 # class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+
+
+class BouncingBall(Ball):
+    def update(self):
+        if not self.position.y > self.bounds[1] - self.radius:
+            self.velocity[1] += 0.8
+        self.velocity *= 0.99
+        super().update()
 
 # class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+
+
+class RainbowBall(Ball):
+    def update(self):
+        r = (self.color[0] + 3) % 256
+        g = (self.color[1] + 7) % 256
+        b = (self.color[2] + 5) % 256
+        self.color = [r, g, b]
+
+        super().update()
+
 
 # class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
 
-# class KineticBall(???):
-#     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
-#     """
-#     # TODO:
+
+class BouncingRainbow(BouncingBall, RainbowBall):
+    def update(self):
+        RainbowBall.update(self)
+        BouncingBall.update(self)
+
+
+class KineticBall(Ball):
+    def __init__(self, bounds, position, velocity, color, radius, shapes):
+        self.shapes = [shape for shape in shapes if hasattr(shape, "radius")]
+        super().__init__(bounds, position, velocity, color, radius)
+
+    def update(self):
+        for shape in self.shapes:
+            distance = (shape.position - self.position).length()
+            if not shape == self and (distance <= self.radius + shape.radius):
+                self_vec = self.velocity
+                shape_vec = shape.velocity
+                self.velocity -= (
+                    (self_vec - shape_vec).dot(self.position - shape.position)
+                    / (self.position - shape.position).length_squared()) * \
+                    (self.position - shape.position)
+                shape.velocity -= (
+                    (shape_vec - self_vec).dot(shape.position - self.position)
+                    / (shape.position - self.position).length_squared()) * \
+                    (shape.position - self.position)
+        super().update()
 
 # class KineticBouncing(???):
 #     """
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     And is affected by gravity
 #     """
-    
+
 
 # class AllTheThings(???):
 #     """
--- src/draw.py
+++ src/draw.py
@@ -1,60 +1,76 @@
-import pygame #TODO:  Fix intellisense
-import random
+import pygame
+# TODO:  Fix intellisense
+# import random
 
 from pygame.math import Vector2
 
-from ball import *
-from block import *
+from ball import Ball, RainbowBall, BouncingBall, BouncingRainbow, KineticBall
+from block import Block
 
 SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
 
-def debug_create_balls(object_list):
+
+def create_balls(object_list):
     ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
     object_list.append(ball)
 
+    ball = RainbowBall(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 6), [255, 255, 0], 20)
+    object_list.extend((ball, ))
+
+    ball = BouncingBall(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 6), [255, 255, 0], 20)
+    object_list.extend((ball, ))
+
+    ball = BouncingRainbow(SCREEN_SIZE, Vector2(30, 60), Vector2(2, 12), [255, 255, 0], 20)
+    object_list.extend((ball, ))
+
+    ball = KineticBall(SCREEN_SIZE, Vector2(30, 60), Vector2(2, 12), [122, 122, 122], 20, object_list)
+    object_list.extend((ball, ))
     # TODO: Create other ball types for testing
-    
-def debug_create_blocks(object_list):
-    block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
+
+
+def create_blocks(object_list):
+    block = Block(SCREEN_SIZE, Vector2(100, 100), 20, 20, [0, 255, 0])
     object_list.extend((block, ))
-  
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
 
-    debug_create_balls(object_list)
-    debug_create_blocks(object_list)
- 
-    while True: # TODO:  Create more elegant condition for loop
+    object_list = []  # list of objects of all types in the toy
+
+    create_balls(object_list)
+    create_blocks(object_list)
+
+    while True:  # TODO:  Create more elegant condition for loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
         # Logic Loop
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:  #TODO:  Get working
+            if event.type == pygame.KEYDOWN:  # TODO:  Get working
                 if event.key == pygame.K_SPACE:
                     # TODO: Add behavior when button pressed
                     pass
 
-        for ball in object_list:
-            ball.update()
- 
+        for shape in object_list:
+            shape.update()
+
         # Draw Loop
         screen.fill(BACKGROUND_COLOR)
-        for ball in object_list:
-            ball.draw(screen, pygame)
- 
+        for shape in object_list:
+            shape.draw(screen, pygame)
+
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
+
 if __name__ == "__main__":
     main()
