39dda4e425d2fe7b4770011a33a8daf551af2023
udykas <udykas@gmail.com>
2018-07-11 18:02:50 -0400

    First commit

--- src/draw.py
+++ src/draw.py
@@ -18,6 +18,9 @@ def debug_create_balls(object_list):
 def debug_create_blocks(object_list):
     block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
     object_list.extend((block, ))
+
+    block = RainbowBlock(SCREEN_SIZE, Vector2(200,100), 60, 30, [255,255,0])
+    object_list.extend((block, ))
   
 def main():
     pygame.init()
@@ -42,8 +45,8 @@ def main():
                     # TODO: Add behavior when button pressed
                     pass
 
-        for ball in object_list:
-            ball.update()
+        for obj in object_list:
+            obj.update()
  
         # Draw Loop
         screen.fill(BACKGROUND_COLOR)
