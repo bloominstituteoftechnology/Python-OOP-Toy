7b892d67ca85f0e2fd81db064a1ac1e81dfb0d63
Zach Campbell <beazlebubbles@gmail.com>
2018-07-11 13:46:17 -0600

    Changes to please flake8

--- src/block.py
+++ src/block.py
@@ -4,6 +4,7 @@ import random
 from pygame.math import Vector2
 from pygame import Rect
 
+
 class Block:
     """
     Base class for square or rectangular object
@@ -21,11 +22,12 @@ class Block:
         pass
 
     def set_rectangle(self, position, width, height):
-        # Creates a rectangle of the given width and height centered at the x/y coordinates
+        # Creates a rectangle of the given width and height centered at the x/y
+        # coordinates
         return pygame.Rect(position.x - (width/2),
                            position.y - (height/2),
-                                    width,
-                                    height)
+                           width,
+                           height)
 
     def draw(self, screen, pygame):
         pygame.draw.rect(screen, self.color, self.rectangle)
