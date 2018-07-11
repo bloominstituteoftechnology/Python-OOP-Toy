99404ab2e588f73b705616d704aba214077380a2
maribelcuales <maribelcuales@gmail.com>
2018-07-12 06:57:12 +0800

    added Bouncing Ball and Rainbow Ball to the pygame

--- src/ball.py
+++ src/ball.py
@@ -25,17 +25,32 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball):
+    """
+    ball effected by gravity
+    """
+    # TODO: create another bouncing ball
+    def update(self): 
+        
+        self.velocity.x *= +1
+        self.velocity.y *= +1
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+        super().update()
+
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    # TODO:
+
+    def update(self):
+        r = (self.color[0] + 3) % 256
+        g = (self.color[1] + 2) % 256
+        b = (self.color[2] - 1) % 256
+        
+        self.color = [r, g, b]
+
+        super().update()
 
 # class BouncingRainbow(???):
 #     """
