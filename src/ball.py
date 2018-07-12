a705fa4f7703558fc4f3ca965112da1836ccc228
samarv <samarvir1996@gmail.com>
2018-07-13 02:23:25 +0530

    added collison

--- src/ball.py
+++ src/ball.py
@@ -34,30 +34,32 @@ class BouncingBall(Ball):
     def update(self):
         #f = mg we don't have a mass 
         # ALSO V = U - gt
-        print(self.velocity)
-        if self.velocity.y > 0.01:
-            self.velocity.y = self.velocity.y - 0.01
-        elif self.velocity.y < 0.01:
-            self.velocity.y = self.velocity.y + 0.01
+        if self.velocity.y > 0.001:
+            self.velocity.y = self.velocity.y - 0.001
+        elif self.velocity.y < 0.001:
+            self.velocity.y = self.velocity.y + 0.001
         else:
-            pass
-            
-        if self.velocity.x > 0.01:
-            self.velocity.x = self.velocity.x - 0.004
-        elif self.velocity.x < 0.01:
-            self.velocity.x = self.velocity.x + 0.004
-        else:
-            pass
-           
+            self.velocity.y = 0
+        
         Ball.update(self)
         
     
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: c
+class BouncingBall2(Ball):
+    """
+    ball effected by gravity
+    """
+    def update(self):
+        self.velocity.y += 0.3
+        Ball.update(self)
 
+class RainbowBall(Ball):
+    def update(self):
+        r =  (self.color[0] + 6) % 256
+        g =  (self.color[1] + 4) % 256
+        b =  (self.color[2] - 2) % 256
+        
+        self.color = [r,g,b]
+        Ball.update(self)
 # class RainbowBall(???):
 #     """
 #     Ball that changes colors
@@ -70,12 +72,40 @@ class BouncingBall(Ball):
 #     """
 #     # TODO:
 
-# class KineticBall(???):
-#     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
-#     """
-#     # TODO:
+class BouncingRainbow(BouncingBall, RainbowBall):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    def update(self):   
+        BouncingBall.update(self)
+        RainbowBall.update(self)
 
+class KineticBall(Ball):
+    """
+    A ball that collides with other collidable balls using simple elastic circle collision
+    """
+    def __init__(self, object_list, bounds, position, velocity, color, radius):
+        self.object_list = object_list
+        super().__init__(bounds, position, velocity, color, radius)
+
+    def update(self):
+        for obj in self.object_list:
+
+            if not issubclass(type(obj), KineticBall):
+                continue
+
+            if obj == self:
+                continue
+
+            distance = obj.position.distance_to(self.position)
+            
+            sumr = self.radius + obj.radius
+
+            if distance < sumr:
+                self.velocity.x *= -1
+                self.velocity.y *= -1
+                print("Collision!")
+        Ball.update(self)      
 # class KineticBouncing(???):
 #     """
 #     A ball that collides with other collidable balls using simple elastic circle collision
