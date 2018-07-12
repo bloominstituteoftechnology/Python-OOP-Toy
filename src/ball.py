d19d88b5ec0d41a6d42b9a90480d5a609ea96a6a
Bradiowave <divebombwalker@gmail.com>
2018-07-12 15:04:32 -0700

    realistic elastic collisions working! (I think...)

--- src/ball.py
+++ src/ball.py
@@ -1,8 +1,10 @@
 import math
+import numpy
 
 from pygame.math import Vector2
 from decimal import *
 from random import *
+from numpy import *
 
 class Ball:
     """
@@ -73,7 +75,6 @@ class BouncingBall(Ball):
         
         self.position += self.velocity
 
-    # TODO: 
 
 class RainbowBall(Ball):
     """
@@ -84,7 +85,9 @@ class RainbowBall(Ball):
         super().__init__(bounds, position, velocity, color, radius)
 
     def update(self):
-        self.color = [int(random() * 255), int(random() * 255), int(random() * 255)]
+        self.color[0] = self.position.x % 255
+        self.color[1] = self.position.y % 255
+        self.color[2] = (self.position.x - self.position.y) % 255
         if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
             self.velocity.x *= -1
         if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
@@ -92,21 +95,119 @@ class RainbowBall(Ball):
         self.position += self.velocity
 
 
-    # TODO:
+class BouncingRainbow(Ball):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    def __init__(self, bounds, position, velocity, color, radius):
+        #big: f(100 * x = 0 , ((100 - 20) / 100) * -1 = -0.2
+        #small: f(0.1) = 1 , ((100 - 10) / 100) * -1 = 
+        self.g = -1 * ((100 - radius) / 100)
+        self.friction = -1
+        super().__init__(bounds, position, velocity, color, radius)
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+    def update(self):
+        # bounce at edges.  TODO: Fix sticky edges
+
+        self.color[0] = self.position.x % 255
+        self.color[1] = self.position.y % 255
+        self.color[2] = (self.velocity.x - int(self.velocity.y)) % 255
+
+        # gravity affecting acceleration
+        if self.g < 0.2 + self.radius:
+            self.velocity.y += 0.2
+        
+        # reduces x direction velocity with friction
+        if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
+            #self.velocity.x *= -1
+            if self.friction < -1.3877787807814457e-16:
+                self.friction += 0.1
+            else:
+                self.friction = 0
+            self.velocity.x *= self.friction
+
+        # reduces y direction velocity with
+        if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
+            if self.friction < -1.3877787807814457e-16:
+                self.friction += 0.1
+            else:
+                self.velocity.x = 0
+            
+            if self.g < 0.2 + self.radius:
+                self.velocity.y = self.velocity.y * self.g
+
+            if self.g < -1.3877787807814457e-16:
+                self.g += 0.1
+
+                
+        if self.position.y > self.bounds[1] - self.radius:
+            self.position.y = self.bounds[1] - self.radius
+        
+        self.position += self.velocity
+
 
 ## STRETCH
 
-# class KineticBall(???):
-#     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
-#     """
-#     # TODO:
+class KineticBall(Ball):
+    """
+    A ball that collides with other collidable balls using simple elastic circle collision
+    """
+    def __init__(self, object_list, bounds, position, velocity, color, radius):
+        self.object_list = object_list
+        super().__init__(bounds, position, velocity, color, radius)
+
+    def update(self):
+        for obj in self.object_list:
+
+            if obj == self:
+                continue
+
+            distance = obj.position.distance_to(self.position)
+
+            sumr = self.radius + obj.radius
+
+            if distance < sumr:
+                print("Collision!")
+                v1 = self.velocity.x
+                v2 = obj.velocity.x
+                m1 = self.radius
+                m2 = obj.radius
+                p1 = self.position.x
+                p2 = obj.position.x
+
+                # self.velocity.x = ( (v1*(m1-m2))+(2*m2*v2) ) / (m1 + m2)
+                # obj.velocity.x = (( (v1*(m1-m2))+(2*m2*v2) ) / (m1 + m2)) * -1
+                
+                # self.velocity.x = v1  -  ((2*m2) / (m1+m2))  *  (numpy.dot( v1-v2 , p1-p2 ) / (numpy.linalg.norm(p1-p2)**2))  *  (p1 - p2)
+
+
+                v1 = self.velocity
+                v2 = obj.velocity
+                m1 = self.radius
+                m2 = obj.radius
+                p1 = self.position
+                p2 = obj.position
+
+                self.velocity = v1  -  ((2*m2) / (m1+m2))  *  (numpy.dot( v1-v2 , p1-p2 ) / (numpy.linalg.norm(p1-p2)**2))  *  (p1 - p2)
+                obj.velocity = v2  -  ((2*m1) / (m1+m2))  *  (numpy.dot( v2-v1 , p2-p1 ) / (numpy.linalg.norm(p2-p1)**2))  *  (p2 - p1)
+
+
+                # v1 = self.velocity.y
+                # v2 = obj.velocity.y
+                # m1 = self.radius
+                # m2 = obj.radius
+                # p1 = self.position.y
+                # p2 = obj.position.y
+
+                # self.velocity.y = ( (v1*(m1-m2))+(2*m2*v2) ) / (m1 + m2)
+                # obj.velocity.y = (( (v1*(m1-m2))+(2*m2*v2) ) / (m1 + m2)) * -1
+                
+                # self.velocity.y = v1  -  ((2*m2) / (m1+m2))  *  (numpy.dot( v1-v2 , p1-p2 ) / (numpy.linalg.norm(p1-p2)**2))  *  (p1 - p2)
+
+
+
+
+        super().update()
 
 # class KineticBouncing(???):
 #     """
--- src/draw.py
+++ src/draw.py
@@ -10,22 +10,58 @@ SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
 
 def debug_create_balls(object_list):
-    ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
-    bouncingball = BouncingBall(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 10)
-    bouncingball2 = BouncingBall(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [0, 255, 255, .5], 50)
-    bouncingball3 = RainbowBall(SCREEN_SIZE, Vector2(200, 200), Vector2(3, 3), [0, 0, 0, .5], 80)
 
-    object_list.append(ball)
-    object_list.append(bouncingball)
-    object_list.append(bouncingball2)
-    object_list.append(bouncingball3)
+    # #BouncingRainbows
+    # bouncingrainbow = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 20)
+    # object_list.append(bouncingrainbow)
+
+    # bouncingrainbow2 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 18)
+    # object_list.append(bouncingrainbow2)
+
+    # bouncingrainbow3 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 16)
+    # object_list.append(bouncingrainbow3)
+
+    # bouncingrainbow4 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 14)
+    # object_list.append(bouncingrainbow4)
+
+    # bouncingrainbow5 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 12)
+    # object_list.append(bouncingrainbow5)
+
+    # bouncingrainbow6 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 10)
+    # object_list.append(bouncingrainbow6)
+
+    # bouncingrainbow7 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 8)
+    # object_list.append(bouncingrainbow7)
+
+    # bouncingrainbow8 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 6)
+    # object_list.append(bouncingrainbow8)
+
+    # bouncingrainbow9 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 4)
+    # object_list.append(bouncingrainbow9)
+
+    # bouncingrainbow10 = BouncingRainbow(SCREEN_SIZE, Vector2(100, 100), Vector2(3, 3), [255, 0, 255, .5], 2)
+    # object_list.append(bouncingrainbow10)
+
+
+    #KineticBalls
+    # kineticball1 = KineticBall(object_list, SCREEN_SIZE, Vector2(100, 300), Vector2(3, 0), [255, 0, 255], 15)
+    # object_list.append(kineticball1)
+
+    kineticball2 = KineticBall(object_list, SCREEN_SIZE, Vector2(500, 300), Vector2(-3, 0), [0, 0, 0], 15)
+    object_list.append(kineticball2)
+
+    # kineticball3 = KineticBall(object_list, SCREEN_SIZE, Vector2(200, 50), Vector2(0, 5), [0, 255, 255], 15)
+    # object_list.append(kineticball3)
+
+    kineticball4 = KineticBall(object_list, SCREEN_SIZE, Vector2(200, 400), Vector2(0, -2), [100, 100, 100], 15)
+    object_list.append(kineticball4)
 
     # TODO: Create other ball types for testing
     
 def debug_create_blocks(object_list):
-    block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
-    object_list.extend((block, ))
-  
+    # block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
+    # object_list.extend((block, ))
+    pass
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
