f21f29c11aa6a00aa51dcc0f64ad71ce9cb475fc
mister-corn <code@ronaldlibago.com>
2018-07-11 17:12:11 -0700

    BouncingBall, RainbowBall, and BouncingRainbow done.

--- src/ball.py
+++ src/ball.py
@@ -25,23 +25,40 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball):
+    """
+    ball affected by gravity
+    """
+    GRAVITY = 0.1
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+    # TODO: 
+    def update(self):
+        self.velocity.y += self.GRAVITY
+        super().update()
+        
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    # TODO:
+    def update(self):
+        r = (self.color[0] + 2) % 256 
+        g = (self.color[1] + 4) % 256
+        b = (self.color[2] + 6) % 256
+
+        self.color = [r, g, b]
+        super().update()
+
+    
+
+class BouncingRainbow(BouncingBall, RainbowBall):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    # TODO:
+    pass
+
 
 # class KineticBall(???):
 #     """
--- src/draw.py
+++ src/draw.py
@@ -10,15 +10,18 @@ SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
 
 def debug_create_balls(object_list):
-    ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
+    ball = BouncingRainbow(SCREEN_SIZE, Vector2(10, 10), Vector2(3, 3), [255, 0, 0], 10)
+    # b_ball = RainbowBall(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [0, 255, 0], 10)
     object_list.append(ball)
+    # object_list.append(b_ball)
 
     # TODO: Create other ball types for testing
     
 def debug_create_blocks(object_list):
-    block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
-    object_list.extend((block, ))
-  
+    # block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
+    # object_list.extend((block, ))
+    pass
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
