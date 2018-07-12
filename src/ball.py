fae625c2e835e0da2d5a56c277deeac694d5799f
jesuarva <jesuarva.developer@gmail.com>
2018-07-12 22:53:08 +0200

    ADD FRICTION: When the ball is not bouncing but rolling on the bottom
    Added friction so the ball slowdown the velocity.x

--- src/ball.py
+++ src/ball.py
@@ -54,7 +54,7 @@ class BouncingBall(Ball):
     def __init__(self, bounds, position, velocity, color, radius, acceleration):
         super().__init__(bounds, position, velocity, color, radius)
         self.acceleration = acceleration
-        self.frictionX = 0.2
+        self.frictionX = 0.01
     
     def update(self):
         self.velocity.y += self.acceleration
@@ -62,13 +62,17 @@ class BouncingBall(Ball):
         nextX = self.position.x + self.velocity.x
         nextY = self.position.y + self.velocity.y
 
-
-        if nextX < 0 + self.radius or nextX > self.bounds[0] - self.radius or nextY < 0 + self.radius or nextY > self.bounds[1] - self.radius:
-            if self.position.y == self.bounds[1] - self.radius: # if ball at bottom
-                if self.velocity.y > -0.3 and self.velocity.y < 0: # if ball velocity is near cero.
+        print(self.velocity.y > -1 and self.velocity.y < 2 and self.position.y == self.bounds[1] - self.radius)
+        if self.position.y == self.bounds[1] - self.radius: # if ball at bottom
+            if self.velocity.y > -1 and self.velocity.y < 2: # if ball velocity is near cero.
+                self.acceleration = 0
+                self.velocity.y = 0
+                if self.velocity.x > 0:
+                    self.velocity.x -= self.frictionX
+                if self.velocity.x < 0:
                     self.velocity.x += self.frictionX
-            else:
-                self.acceleration += self.acceleration * 0.6
+        elif nextX < 0 + self.radius or nextX > self.bounds[0] - self.radius or nextY < 0 + self.radius or nextY > self.bounds[1] - self.radius:
+            self.acceleration += self.acceleration * 0.6
                         
         print('position',self.position.y)
         print('velocity',self.velocity.y)
