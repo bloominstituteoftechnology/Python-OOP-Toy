c805bb504de7a15a3d370d6947758cb145334065
april <martinezapril56@yahoo.com>
2018-07-12 18:03:45 -0400

    working on making things move

--- src/ball.py
+++ src/ball.py
@@ -12,6 +12,7 @@ class Ball:
         self.bounds = bounds
         self.color = color
         self.radius = radius
+        self.num = 8
 
     def update(self):
         # bounce at edges.  TODO: Fix sticky edges
@@ -25,9 +26,17 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
+
+class BouncingBall(Ball):
+    """
+    ball affected by gravity
+    """
+    GRAVITY = 0.1
+
+    # TODO: 
+    def update(self):
+        self.velocity.y += self.GRAVITY
+        super().update()
 #     """
 #     # TODO: 
 
@@ -36,18 +45,53 @@ class Ball:
 #     Ball that changes colors
 #     """
 #     # TODO:
+class Rainbow(Ball):
 
-# class BouncingRainbow(???):
+def update(self):
+    r = (self.color[0] + 3) % 256
+    g = (self.color[1] + 3) % 256
+    b = (self.color[2] + 3) % 256
+
+    self.color = [r, g, b]
+    super().update()
+#super is hertiting class
+
+
+class BouncingRainbow(BouncingBall, RainbowBall):
+    pass
 #     """
 #     Ball that changes color and is affected by gravity
 #     """
 #     # TODO:
 
 # class KineticBall(???):
-#     """
-#     A ball that collides with other collidable balls using simple elastic circle collision
-#     """
-#     # TODO:
+#      A ball that collides with other collidable balls using simple elastic circle collision
+    
+    def __init__(self, mass, object_list, bounds, position, velocity, color, radius):
+        self.object_list = object_list
+        self.mass = mass
+        super().__init__(bounds, position, velocity, color, radius)
+
+    def collide(self, object, relative_vector):
+        # print('bang!')
+        
+        # We can imagine the point of reflection as a wall tangent to the collision 
+        tangent = math.atan2(relative_vector.y, relative_vector.x)
+        # Get the angle of travel for both
+        angle1 = 0.5 * math.pi - math.atan2(self.velocity.y, self.velocity.x)
+        angle2 = 0.5 * math.pi - math.atan2(object.velocity.y, object.velocity.x)
+        # The angles of travel are updated to be two times the tangent minus the current angle
+        angle1 = 2 * tangent - angle1
+        angle2 = 2 * tangent - angle2
+
+        # Exchange speed
+        # Get velocity of other particle
+        object_speed = object.velocity.length()
+        self_speed = self.velocity.length()
+
+        # Update with new angle and opposing particle's speed
+        self.velocity = Vector2(math.sin(angle1), math.cos(angle1)) * object_speed
+        object.velocity = Vector2(math.sin(angle2), math.cos(angle2)) * self_speed
 
 # class KineticBouncing(???):
 #     """
