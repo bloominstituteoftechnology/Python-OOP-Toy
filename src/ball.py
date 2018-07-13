6d9c4a35ae6277548206d3a73e51000682e62ebc
jesuarva <jesuarva.developer@gmail.com>
2018-07-13 12:43:34 +0200

    COLLIDIN BALLS: Implement starting collision.
    TODO: improve the dircetion changes after collision.

--- src/ball.py
+++ src/ball.py
@@ -3,10 +3,12 @@ import random
 
 from pygame.math import Vector2
 
+
 class Ball:
     """
     base class for bouncing objects
     """
+
     def __init__(self, bounds, position, velocity, color, radius):
         self.position = position
         self.velocity = velocity
@@ -18,7 +20,6 @@ class Ball:
         # bounce at edges.  TODO: Fix sticky edges
         nextX = self.position.x + self.velocity.x
         nextY = self.position.y + self.velocity.y
-
 
         if nextX < 0 + self.radius:
             nextX = self.radius
@@ -33,39 +34,42 @@ class Ball:
         if nextY > self.bounds[1] - self.radius:
             nextY = self.bounds[1] - self.radius
             self.velocity.y *= -1
-            
-
 
         # if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
         # if self.position.y < 0 + self.radius or self.position.y > self.bounds[1] - self.radius: # screen height
 
-        
         self.position.x = nextX
         self.position.y = nextY
 
     def draw(self, screen, pygame):
         # cast x and y to int for drawing
-        pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
+        pygame.draw.circle(screen, self.color, [int(
+            self.position.x), int(self.position.y)], self.radius)
+
 
 class BouncingBall(Ball):
     """
     ball effected by gravity
     """
-    # TODO: 
+    # TODO:
+
     def __init__(self, bounds, position, velocity, color, radius, acceleration):
         super().__init__(bounds, position, velocity, color, radius)
         self.acceleration = acceleration
         self.frictionX = 0.01
-    
+
     def update(self):
         self.velocity.y += self.acceleration
 
         nextX = self.position.x + self.velocity.x
         nextY = self.position.y + self.velocity.y
 
-        if self.position.y == self.bounds[1] - self.radius: # if ball at bottom
-            if self.velocity.y > -1 and self.velocity.y < 10: # if ball's velocity.y is near cero.
-                if self.velocity.x > -0.1 and self.velocity.x < 0.1: # if ball's velocity.x is near cero.
+        # if ball at bottom
+        if self.position.y == self.bounds[1] - self.radius:
+            # if ball's velocity.y is near cero.
+            if self.velocity.y > -1 and self.velocity.y < 10:
+                # if ball's velocity.x is near cero.
+                if self.velocity.x > -0.1 and self.velocity.x < 0.1:
                     # STOP movement on X axis.
                     self.velocity.x = 0
                     self.frictionX = 0
@@ -78,31 +82,33 @@ class BouncingBall(Ball):
                         self.velocity.x -= self.frictionX
                     if self.velocity.x < 0:
                         self.velocity.x += self.frictionX
-        elif  nextY < 0 + self.radius or nextY > self.bounds[1] - self.radius: # If ball hit Y bounds
+        # If ball hit Y bounds
+        elif nextY < 0 + self.radius or nextY > self.bounds[1] - self.radius:
             self.acceleration += self.acceleration * 0.6
             if self.velocity.x > 0:
                 self.velocity.x -= self.frictionX
             if self.velocity.x < 0:
                 self.velocity.x += self.frictionX
-                        
+
         # print('position',self.position.x)
         # print('velocity',self.velocity.x)
 
         super().update()
 
-        
-    
+
 class RainbowBall(Ball):
     """
     Ball that changes colors
     """
     # TODO:
+
     def update(self):
-        r = (self.color[0] + random.randrange(0,10)) % 256
-        g = (self.color[1] + random.randrange(0,10)) % 256
-        b = (self.color[2] + random.randrange(0,10)) % 256
+        r = (self.color[0] + random.randrange(0, 10)) % 256
+        g = (self.color[1] + random.randrange(0, 10)) % 256
+        b = (self.color[2] + random.randrange(0, 10)) % 256
         self.color = [r, g, b]
         super().update()
+
 
 class BouncingRainbow(BouncingBall, RainbowBall):
     """
@@ -113,17 +119,18 @@ class BouncingRainbow(BouncingBall, RainbowBall):
     #     super().__init__(bounds, position, velocity, color, radius, acceleration)
     pass
 
+
 class KineticBall(Ball):
     """
     A ball that collides with other collidable balls using simple elastic circle collision
     """
     # TODO:
-    def collide(self, distanceToObject, sumOfRadius):
-        # self.velocity *= -1
-        print('Collision!')
-        
-        self.update()
 
+    def collide(self, distanceToObject, sumOfRadius):
+        self.velocity *= -1
+        print('Collision!')
+
+        self.update()
 
 
 # class KineticBouncing(???):
@@ -131,7 +138,7 @@ class KineticBall(Ball):
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     And is affected by gravity
 #     """
-    
+
 
 # class AllTheThings(???):
 #     """
--- src/draw.py
+++ src/draw.py
@@ -1,4 +1,4 @@
-import pygame #TODO:  Fix intellisense
+import pygame  # TODO:  Fix intellisense
 import random
 
 from pygame.math import Vector2
@@ -8,62 +8,91 @@ from block import *
 
 SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
-gravityAcceleration = 0.15
+GRAVITY_ACCELERATION = 0.15
+
 
 def debug_create_balls(object_list):
     ball = Ball(SCREEN_SIZE, Vector2(50, 50), Vector2(3, 3), [255, 0, 0], 10)
     object_list.append(ball)
-    ball = KineticBall(SCREEN_SIZE, Vector2(50, 50), Vector2(3, -5), [0, 0, 0], 30)
+    # TODO: Create other ball types for testing
+    ball = KineticBall(SCREEN_SIZE, Vector2(50, 50),
+                       Vector2(3, -5), [0, 255, 0], 30)
     object_list.append(ball)
-    ball = KineticBall(SCREEN_SIZE, Vector2(50, 50), Vector2(-3, 3), [0, 0, 0], 30)
+    ball = KineticBall(SCREEN_SIZE, Vector2(550, 50),
+                       Vector2(-3, 3), [255, 0, 0], 30)
     object_list.append(ball)
 
-    # TODO: Create other ball types for testing
-    ball = BouncingBall(SCREEN_SIZE, Vector2(50, 40), Vector2(3, 0), [0, 250, 0], 10, gravityAcceleration)
+    ball = BouncingBall(SCREEN_SIZE, Vector2(50, 40), Vector2(
+        3, 0), [0, 250, 0], 10, GRAVITY_ACCELERATION)
     object_list.append(ball)
-    ball = RainbowBall(SCREEN_SIZE, Vector2(50, 70), Vector2(3, 1), [0, 0, 250], 10)
+    ball = RainbowBall(SCREEN_SIZE, Vector2(50, 70),
+                       Vector2(3, 1), [0, 0, 250], 10)
     object_list.append(ball)
-    ball = BouncingRainbow(SCREEN_SIZE, Vector2(50, 70), Vector2(3, -4), [0, 250, 0], 20, gravityAcceleration)
+    ball = BouncingRainbow(SCREEN_SIZE, Vector2(50, 70), Vector2(
+        3, -4), [0, 250, 0], 20, GRAVITY_ACCELERATION)
     object_list.append(ball)
-    
+
+
 def debug_create_blocks(object_list):
-    block = Block(SCREEN_SIZE, Vector2(100,100), 20, 20, [0,255,0])
+    block = Block(SCREEN_SIZE, Vector2(100, 100), 20, 20, [0, 255, 0])
     object_list.extend((block, ))
-  
+
+
 def debug_start_game(object_list):
-   debug_create_balls(object_list)
-   debug_create_blocks(object_list)
+    debug_create_balls(object_list)
+    debug_create_blocks(object_list)
+
+
+def slopes(obj, nextObjectPosition):
+    objX = obj.position.x
+    objY = obj.position.y
+    itemX = nextObjectPosition.x
+    itemY = nextObjectPosition.y
+    if (objX - itemX) == 0:
+        slope = 0
+    else:
+        slope = (objY - itemY) / (objX - itemX)
+    return slope
+
 
 def detectCollision(obj, colliding_objs):
     '''If there are collision return true'''
     for item in colliding_objs:
-        if not obj == item: # If obj is not itself
+        if not obj == item:  # If obj is not itself
             obj_radius = obj.radius
             item_radius = item.radius
             sumOfRadius = obj_radius + item_radius
             nextObjPosition = obj.position + obj.velocity
             nextItemPosition = item.position + item.velocity
             distance = nextObjPosition.distance_to(nextItemPosition)
-            if distance <= sumOfRadius:
-                obj.collide(distance, sumOfRadius)
 
+            if distance <= sumOfRadius:
+                objSlope = slopes(obj, nextObjPosition)
+                itemSlope = slopes(item, nextItemPosition)
+
+                return True
+
+            elif distance == sumOfRadius:
+                pass
+            else:
+                return False
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
+
+    object_list = []  # list of objects of all types in the toy
     Kinetic_List = []
- 
-    while True: # TODO:  Create more elegant condition for loop
+
+    while True:  # TODO:  Create more elegant condition for loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            if event.type == pygame.KEYDOWN:  #TODO:  Get working
+            if event.type == pygame.KEYDOWN:  # TODO:  Get working
                 if event.key == pygame.K_SPACE:
                     # TODO: Add behavior when button pressed
                     print(event)
@@ -71,34 +100,35 @@ def main():
                     debug_start_game(object_list)
                     for obj in object_list:
                         if isinstance(obj, KineticBall):
-                            Kinetic_List.append(obj) 
+                            Kinetic_List.append(obj)
                 if event.key == pygame.K_q:
                     pygame.quit()
-                
 
         print('Kinetic_List', len(Kinetic_List))
         for obj in object_list:
             # Detect collision -> if collision set new variables in Objs
-            if isinstance(obj, KineticBall): #
+            if isinstance(obj, KineticBall):
                 # print('isinstance')
                 if detectCollision(obj, Kinetic_List):
-                    obj.collide()
+                    obj.collide(0, 0)
+                    pass
                 else:
                     obj.update()
             else:
                 # Paint new state
                 obj.update()
- 
+
         # Draw Loop
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
             ball.draw(screen, pygame)
- 
+
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
+
 if __name__ == "__main__":
     main()
