51d031f18bc686c07f71a84d14e7b4ba6942a0bc
JasonCorp84 <csababalogh84@gmail.com>
2018-07-23 09:38:46 -0700

    gravity added, MVP done

---
+++ NOTES.md
@@ -0,0 +1,13 @@
+# OOP With Python
+
+## Starting the project
+
+    pipenv install
+    pipenv shell
+
+## Day1
+
+We work on the ball.py file.
+
+
+    super().update() so it inherits from the other class also.
--- src/ball.py
+++ src/ball.py
@@ -1,5 +1,7 @@
 import math
 
+
+
 from pygame.math import Vector2
 
 class Ball:
@@ -12,7 +14,7 @@ class Ball:
         self.bounds = bounds
         self.color = color
         self.radius = radius
-
+        print(self.position.x)
     def update(self):
         # bounce at edges.  TODO: Fix sticky edges
         if self.position.x < 0 + self.radius or self.position.x > self.bounds[0] - self.radius: # screen width
@@ -25,38 +27,47 @@ class Ball:
         # cast x and y to int for drawing
         pygame.draw.circle(screen, self.color, [int(self.position.x), int(self.position.y)], self.radius)
 
-# class BouncingBall(???):
-#     """
-#     ball effected by gravity
-#     """
-#     # TODO: 
+class BouncingBall(Ball):
+    """
+    ball effected by gravity
+    """
+    def update(self):
+        
+        self.velocity.y +=  0.1 # my balls were so fast so using this one instead
+        print(self.velocity.y)
+            
+        
+        super().update()
 
-# class RainbowBall(???):
-#     """
-#     Ball that changes colors
-#     """
-#     # TODO:
+class RainbowBall(Ball):
+    """
+    Ball that changes colors
+    """
+    def update(self):
+        r = (self.color[0] + 3) % 256
+        g = (self.color[1] + 2) % 256
+        b = (self.color[2] - 1) % 256
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+        self.color = [r, g, b]
+        super().update() # we have to call the functionalites from the original class, otherwise it will be overwritten
 
-# class KineticBall(???):
+class BouncingRainbow(BouncingBall, RainbowBall):
+    pass
+
+# class KineticBall(Ball):
 #     """
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     """
 #     # TODO:
+#     def update(self):
+
 
 # class KineticBouncing(???):
 #     """
 #     A ball that collides with other collidable balls using simple elastic circle collision
 #     And is affected by gravity
 #     """
-    
+
 
 # class AllTheThings(???):
-#     """
-#     A ball that does everything!
-#     """
+# A ball that does everything!
